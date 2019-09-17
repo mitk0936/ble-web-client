427dab2537d83ae5576e15cbcd76e85b9ace8706
Dimitar Mihaylov <dimitar.mihaylov@siteground.com>
2019-09-17 14:03:12 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import './test';
+import hm10 from './hm-10';
 
-const asdf = `ok${+new Date()}asdasd`;
+window.start = function () {
+  const client = new hm10({ onDisconnect: () => alert('Disconnected') });
+  
+  client()
+    .then(({ write, subscribe }) => {
+      console.log(res);
+      setInterval(() => {
+        write(`${+new Date()}\r\n`);
+      }, 5700);
+
+      subscribe(console.log);
+    })
+    .catch(console.error);
+};
+
--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,40 @@ var webpack = require('webpack');
 var Visualizer = require('webpack-visualizer-plugin');
 
 module.exports = {
-entry: './src/index.js',
-output: {
-  path: path.resolve(__dirname, 'lib'),
-  filename: 'app.bundle.js'
-},
-module: {
-  rules: [
-    {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      exclude: [/node_modules/],
-      query: {
-        presets: [["@babel/preset-env", {
-          "useBuiltIns": "usage",
-          "targets": "> 0.25%, not dead"
-        }]]
-      }
-    },
-  ]
-},
-stats: {
-  colors: true
-},
-plugins: [new Visualizer({
-  filename: './statistics.html'
-})],
-devtool: 'source-map',
-devServer: {
-  contentBase: path.join(__dirname, 'lib'),
-  compress: true,
-  port: 9000,
-  hot: true
-}
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: [/node_modules/],
+        query: {
+          presets: [["@babel/preset-env", {
+            "useBuiltIns": "usage",
+            "targets": "> 0.25%, not dead"
+          }]]
+        }
+      },
+    ]
+  },
+  stats: {
+    colors: true
+  },
+  plugins: [new Visualizer({
+    filename: './statistics.html'
+  })],
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'lib'),
+    compress: true,
+    host: '192.168.8.101',
+    https: true,
+    port: 9000,
+    hot: true
+  }
 };
